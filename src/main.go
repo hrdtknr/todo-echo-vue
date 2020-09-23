70deb6b0c0430c20e347457ef4ab9e794824d056
rino_pk <rino@pk.com>
2020-09-23 11:56:31 +0900

    1156

--- src/main.go
+++ src/main.go
@@ -33,58 +33,27 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	//	e.Use(middleware.CORS())//AccessControl
-	/*
-	   e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	   		AllowOrigins: []string{"http://localhost:8000/", "http://localhost:8080/"},
-	   		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-	   	}))
-	*/
-	/*
-		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	    AllowOrigins:     []string{"*"},
-	    AllowHeaders:     []string{"authorization", "Content-Type"},
-	    AllowCredentials: true,
-	    AllowMethods:     []string{echo.OPTIONS, echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
-	}))
-	*/
-	e.Use(func(h echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			req := c.Request()
+	e.Use(middleware.CORS())
 
-			headers := c.Response().Header()
-			headers.Set("Access-Control-Allow-Origin", "*")
-			headers.Set("Access-Control-Allow-Headers", "Authorization")
-			headers.Set("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS")
-
-			if "OPTIONS" != req.Method {
-				h(c)
-			}
-
-			return nil
-		}
-	})
 	// Routes
 	e.GET("/todoList", handler)
 	e.DELETE("/todoList", deleteTodo)
-	e.POST("/todoList", saveTodo) // "/"にしたけど違いそう
+	e.POST("/todoList", saveTodo)
+	e.PUT("/todoList", updateTodo)
+
 	// Start server
 	e.Logger.Fatal(e.Start(":8000"))
 }
 
 func handler(c echo.Context) error {
-	log.Println("handler") // insert delete ここにきてる
+	log.Println("handler") // insert ここにきてる
 	return c.JSON(http.StatusOK, todoList)
 }
 
 func deleteTodo(c echo.Context) error {
 	log.Println("delete")
-	//	log.Println(c.Param("Id"))
-	id2 := c.QueryParam("id") //クエリパラメータで送ってるから受け取り方がc.Paramではない
-	log.Println("query id2:", id2)
-
-	id, _ := strconv.Atoi(c.Param("Id"))
-	log.Println("id:", id)
+	id, _ := strconv.Atoi(c.QueryParam("id"))
+	del(id)
 	return c.NoContent(http.StatusNoContent)
 }
 
@@ -96,6 +65,16 @@ func saveTodo(c echo.Context) error {
 		return err
 	}
 	return c.JSON(http.StatusOK, t)
+}
+
+func updateTodo(c echo.Context) error {
+	todo := new(Todo)
+	if err := c.Bind(todo); err != nil {
+		return err
+	}
+	log.Println("todo", todo)
+	upd(todo)
+	return nil // 仮
 }
 
 func getTodos() {
@@ -121,4 +100,23 @@ func getTodos() {
 
 	engine.ShowSQL(true)
 	engine.SetMapper(core.GonicMapper{})
+}
+
+func del(id int) {
+	t := Todo{}
+	log.Println("engine", engine) //渡ってない
+	affected, err := engine.Where("id=?", id).Delete(&t)
+	log.Println("del3")
+	if err != nil {
+		log.Println(affected, err)
+	}
+	log.Println("del4")
+}
+
+func upd(todo *Todo) {
+	log.Println("engine", engine)
+	affected, err := engine.Where("id=?", todo.Id).Update(&todo)
+	if err != nil {
+		log.Println(affected, err)
+	}
 }
